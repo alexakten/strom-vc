ea7373279bcfa7e15b7977c8d73de3412c2d5ca2
Alex <alex.akten@outlook.com>
2024-02-27 15:06:25 +0100

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ export const metadata: Metadata = {
   description: "Founders that accelerate founders",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className={GeistSans.className}>
       <body>{children}</body>
