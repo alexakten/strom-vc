fe38f27a04e7acfaf80995d6e01a2a6bb1128d6b
Alex <alex.akten@outlook.com>
2024-02-27 15:05:41 +0100

    text sizes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 export default function Navbar() {
   return (
-    <nav className="absolute flex justify-between top-0 w-full px-4 py-4">
-      <p className="text-3xl text-black">strōm</p>
+    <nav className="absolute top-0 flex w-full justify-between px-8 py-4">
+      <p className="text-4xl tracking-tight text-black">strōm</p>
       <Link href={""}>
-        <button className="flex items-center gap-2 rounded-full border border-gray-200 bg-black px-4 py-2">
-          <div className="h-2 w-2 rounded-full bg-green-400"></div>
-          <p className="text-white">Get in touch -&gt;</p>
+        <button className="flex group items-center gap-2 rounded-full border border-gray-200 bg-black px-4 py-2">
+          <div className="h-2 w-2 transform rounded-full bg-green-400 transition-all duration-300 ease-in-out  group-hover:scale-150"></div>
+          <p className="text-sm text-white">Get in touch -&gt;</p>
         </button>
       </Link>
     </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main className="flex h-[100svh] items-center justify-center bg-gray-100">
+    <main className="flex h-[100svh] items-center justify-center bg-gray-100 px-4">
       <Navbar />
       <div className="flex max-w-4xl flex-col items-center gap-4 text-black">
         <div className="flex items-center gap-2 rounded-full border border-gray-200 bg-white px-4 py-1">
           <div className="h-2 w-2 rounded-full bg-green-400"></div>
           <p className="text-sm">Launching soon</p>
         </div>
-        <h1 className="text-center text-8xl tracking-tighter">
-          Founders that accelerate founders.
+        <h1 className="text-center text-[clamp(40px,7vw,96px)] leading-[1.05] tracking-tighter">
+          Founders that <br /> accelerate founders.
         </h1>
         <p className="max-w-lg text-center text-lg opacity-50">
           Strōm is a community for founders by founders. Get access to talent
-          and capital from startup founders just like you.
+          and capital from startup founders worldwide.
         </p>
         <Link href={""}>
-          <button className="flex items-center gap-2 rounded-full border border-gray-200 bg-black px-6 py-3">
-            <div className="h-2 w-2 rounded-full bg-green-400"></div>
+          <button className="group flex items-center gap-2 rounded-full border border-gray-200 bg-black px-6 py-3 transition duration-300 ease-in-out">
+            <div className="h-2 w-2 transform rounded-full bg-green-400 transition-all duration-300 ease-in-out  group-hover:scale-150"></div>
             <p className="text-white">Join waitlist -&gt;</p>
           </button>
         </Link>
